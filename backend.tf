1fc74c00dcd47712631799b6ef6c15a5f32bb7b8
Chaitanya6746 <104977275+Chaitanya6746@users.noreply.github.com>
2024-02-29 22:37:30 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-bucket-2702"
+    bucket = "terraform-state-bucket-01"
     key = "main"
     region = "ap-south-1"
-    dynamodb_table = "hashtekTerraformStatelock"
+    dynamodb_table = "SaiTerraformStatelock"
   }
 }
